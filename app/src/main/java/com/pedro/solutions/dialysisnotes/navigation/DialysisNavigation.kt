d549d342d7fd396a16a50def37f9229923599ff9
Pedro de Torres Maschio <pedrotorres_maschio@hotmail.com>
2024-06-01 15:35:14 -0300

    Refactor the Navigation logic and remove the drawer (unused)

---
+++ app/src/main/java/com/pedro/solutions/dialysisnotes/navigation/DialysisNavigation.kt
@@ -0,0 +1,194 @@
+package com.pedro.solutions.dialysisnotes.navigation
+
+import androidx.compose.animation.AnimatedContentTransitionScope
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.expandIn
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.List
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.IntSize
+import androidx.navigation.NavController
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.pedro.solutions.dialysisnotes.R
+import com.pedro.solutions.dialysisnotes.ui.add_edit.AddEditDialysis
+import com.pedro.solutions.dialysisnotes.ui.add_edit.DialysisViewModel
+import com.pedro.solutions.dialysisnotes.ui.dialysis_list.DialysisList
+import com.pedro.solutions.dialysisnotes.ui.login.LoginScreen
+import com.pedro.solutions.dialysisnotes.ui.login.LoginViewModel
+import com.pedro.solutions.dialysisnotes.ui.pdf_generator.AddEditPDF
+import com.pedro.solutions.dialysisnotes.ui.pdf_generator.PDFList
+import com.pedro.solutions.dialysisnotes.ui.pdf_generator.PDFViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(
+    dialysisViewModel: DialysisViewModel,
+    pdfViewModel: PDFViewModel,
+    loginViewModel: LoginViewModel
+) {
+    val navController = rememberNavController()
+    val startDestination: String =
+        if (loginViewModel.isUserLoggedIn()) DialysisDestination.MainScreen.route else DialysisDestination.LoginScreen.route
+
+    val bottomNavigationItems =
+        listOf(DialysisDestination.MainScreen, DialysisDestination.PDFList)
+    val bottomNavigationIcons = listOf(
+        Icons.Filled.List, Icon(
+            painter = painterResource(id = R.drawable.pdf_icon), contentDescription = null
+        )
+    )
+    var selectedNavigationItem by remember {
+        mutableIntStateOf(0)
+    }
+
+    val onClickFloatingButton: (destination: DialysisDestination) -> Unit = {
+        when (it) {
+            is DialysisDestination.AddEditDialysis -> navController.navigate(DialysisDestination.AddEditDialysis.route + "/-1")
+            is DialysisDestination.AddEditPDF -> navController.navigate(DialysisDestination.AddEditPDF.route + "/-1")
+            else -> navController.navigate(DialysisDestination.AddEditDialysis.route + "/-1")
+
+        }
+    }
+
+    Scaffold(topBar = {
+        TopAppBar(title = { Text(text = stringResource(R.string.app_name)) })
+    }, floatingActionButton = {
+        FloatingButton(
+            navController = navController, onClick = onClickFloatingButton
+        )
+    }, bottomBar = {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentDestination = navBackStackEntry?.destination
+        val route = currentDestination?.route
+        if (route != null && route != DialysisDestination.LoginScreen.route) {
+            NavigationBar {
+                bottomNavigationItems.forEachIndexed { index, screen ->
+                    NavigationBarItem(selected = selectedNavigationItem == index,
+                        onClick = {
+                            selectedNavigationItem = index
+                            navController.navigate(screen.route)
+                        },
+                        // TODO: find out why this icon it is not showing
+                        icon = {
+                            bottomNavigationIcons[index]
+                        },
+
+                        label = { Text(text = stringResource(screen.resourceId)) })
+                }
+            }
+        }
+    }, modifier = Modifier.fillMaxSize()
+    ) { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = startDestination,
+            enterTransition = {
+                fadeIn(
+                    tween(200)
+                )
+            },
+            exitTransition = {
+                fadeOut(tween(200))
+            },
+            popEnterTransition = {
+                slideIntoContainer(
+                    AnimatedContentTransitionScope.SlideDirection.End,
+                    tween(700)
+                )
+            }
+        ) {
+            composable(
+                DialysisDestination.AddEditDialysis.route + "/{userId}",
+                arguments = listOf(navArgument("userId") {
+                    type = NavType.IntType
+                    defaultValue = -1
+                })
+            ) {
+                val id = it.arguments?.getInt("userId")
+                dialysisViewModel.loadDialysisItem(id)
+                AddEditDialysis(
+                    dialysisViewModel,
+                    onSaveOrDeleteButtonSelected = { destination ->
+                        navController.navigate(destination.route)
+                    })
+            }
+            composable(DialysisDestination.MainScreen.route) {
+                DialysisList(
+                    dialysisViewModel.dialysisList.observeAsState(initial = listOf()).value,
+                    innerPadding,
+                    onItemClicked = { itemId ->
+                        navController.navigate(DialysisDestination.AddEditDialysis.route + "/$itemId")
+                    })
+            }
+            composable(DialysisDestination.PDFList.route) {
+                pdfViewModel.resetState()
+                PDFList(
+                    pdfViewModel.allPDFsGenerated.observeAsState(initial = listOf()).value,
+                    innerPadding
+                )
+            }
+            composable(DialysisDestination.AddEditPDF.route + "/{PDFId}") {
+                AddEditPDF(pdfViewModel) {
+                    navController.navigate(it.route)
+                }
+            }
+
+            composable(DialysisDestination.LoginScreen.route) {
+                LoginScreen(loginViewModel = loginViewModel) {
+                    navController.navigate(DialysisDestination.MainScreen.route)
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FloatingButton(navController: NavController, onClick: (DialysisDestination) -> Unit) {
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentDestination = navBackStackEntry?.destination?.route
+    val onClickDestination = when (currentDestination) {
+        DialysisDestination.MainScreen.route -> DialysisDestination.AddEditDialysis
+        DialysisDestination.PDFList.route -> DialysisDestination.AddEditPDF
+        else -> DialysisDestination.AddEditDialysis
+    }
+
+    val visibleDestinations =
+        listOf(DialysisDestination.MainScreen.route, DialysisDestination.PDFList.route)
+
+
+    val floatingVisibility =
+        currentDestination == null || visibleDestinations.contains(currentDestination)
+
+    AnimatedVisibility(visible = floatingVisibility,
+        enter = fadeIn() + expandIn { IntSize(width = 1, height = 1) }) {
+        FloatingActionButton(onClick = { onClick(onClickDestination) }) {
+            Icon(Icons.Filled.Add, stringResource(R.string.create_new_dialysis))
+        }
+    }
+}
